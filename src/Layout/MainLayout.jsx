d6c63fac7b5cf27712b684fd966d372531d323ac
mati-gif <matiassoria7700@gmail.com>
2024-09-23 14:02:59 -0300

    se cambio el nombre del impor nav del componente mainLayout

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import { Outlet } from 'react-router-dom'
-import Nav from '../components/nav';
+import Nav from '../components/Nav';
 
 
 function MainLayout({ children }) {
